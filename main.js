b9558a1f905f16ddeb11c39f889bfb2be487305f
SyedSabir895 <sabirsyed2005@gmail.com>
2024-07-12 11:51:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// logical operators - used to combine or manipualte boolean operators
+
+// AND operator (&&)  the two conditions should be adsolutely true
+
+// or operator (||)  alteast one condtiton should be true
+
+// not operator (!) it changes true to false and false to true
+
+//const temp=25;
+
+/*if(temp>0 && temp<30){
+    console.log("Temperature is GOOD");
+}
+
+else{
+    console.log("Temperature is BAD");
+} */
+
+/*if(temp<=0 || temp>30){
+    console.log("Temperature is BAD");
+}
+else{
+    console.log("Temperature is GOOD");
+}  */
+
+const raining = false;
+
+if(!raining){
+    console.log("it is not raining");
+}
+else{
+    console.log("it is raining");
+}
